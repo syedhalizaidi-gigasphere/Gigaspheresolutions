1e9b6db400e7fae8d9bdae650fa0ba3e06df3630
syedhaideralizaidi <haider@gigaspheresolutions.com>
2025-07-03 20:05:32 +0500

    minor fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+//
+// // https://vite.dev/config/
+// export default defineConfig(({ command }) => {
+//   const isProd = command === 'build';
+//   return {
+//     base: isProd ? '/Gigaspheresolutions/' : '/Gigaspheresolutions/',
+//     plugins: [react()],
+//   };
+// });
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig(({ command }) => {
   const isProd = command === 'build';
   return {
-    base: isProd ? '/Gigaspheresolutions/' : '/Gigaspheresolutions/',
+    base: isProd ? '/' : '/',
     plugins: [react()],
   };
 });
